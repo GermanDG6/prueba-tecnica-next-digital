dba3baac84a13d8d702f02f0514803501634263a
GermanDG <gdelgado@civitfun.com>
2025-08-14 10:30:23 +0100

    refactor: extract mock to setup-test file

--- setup-test.ts
+++ setup-test.ts
@@ -1,1 +1,7 @@
 import '@testing-library/jest-dom'
+
+import { mock } from 'vitest-mock-extended'
+import type { UserRepository } from './src/features/user/domain/user-repository'
+import { UserLocator } from './src/features/user/ui/di/user-locator'
+
+UserLocator.testRepository = mock<UserRepository>()
--- src/features/user/ui/di/user-locator.ts
+++ src/features/user/ui/di/user-locator.ts
@@ -1,16 +1,20 @@
-import { mock } from 'vitest-mock-extended'
 import { GetUsersQuery } from '../../application/get-users.query'
 import type { UserRepository } from '../../domain/user-repository'
 import { HttpUserRepository } from '../../infrastructure/http-user-repository'
 
 export class UserLocator {
-  static testRepository = mock<UserRepository>()
+  static testRepository?: UserRepository
   private static readonly isTestMode = process.env.NODE_ENV === 'test'
 
-  private static repository = UserLocator.isTestMode
-    ? this.testRepository
-    : new HttpUserRepository()
-
+  private static get repository(): UserRepository {
+    if (UserLocator.isTestMode) {
+      if (!UserLocator.testRepository) {
+        throw new Error('testRepository no inicializado en test')
+      }
+      return UserLocator.testRepository
+    }
+    return new HttpUserRepository()
+  }
   static getUsersQry() {
     return new GetUsersQuery(this.repository)
   }
