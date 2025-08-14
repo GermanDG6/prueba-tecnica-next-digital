0a58a26df5a4f1862901e741b1ddb97ad6b7834c
GermanDG <gdelgado@civitfun.com>
2025-08-14 10:17:23 +0100

    feat: add users page

---
+++ src/features/user/ui/users.page.tsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from 'react'
+import type { User } from '../domain/user'
+import { UserCard } from './components/user-card'
+import { UserLocator } from './di/user-locator'
+
+export const UsersPage = () => {
+  const [users, setUsers] = useState<User[]>([])
+
+  const getUsersQry = UserLocator.getUsersQry()
+
+  const getUsers = async () => {
+    const users = await getUsersQry.execute()
+    setUsers(users)
+  }
+
+  useEffect(() => {
+    getUsers()
+  }, [])
+
+  return (
+    <>
+      <h1>Users</h1>
+
+      {users.length > 0 ? (
+        users.map((user) => {
+          return (
+            <a href={`/users/${user.id}`}>
+              <UserCard user={user} />
+            </a>
+          )
+        })
+      ) : (
+        <p>No users</p>
+      )}
+    </>
+  )
+}
