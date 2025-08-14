d528495c95b839abae847602cd643c27756f5b9b
GermanDG <gdelgado@civitfun.com>
2025-08-14 11:31:21 +0100

    test: add user page test

---
+++ src/features/user/ui/user.page.test.tsx
@@ -0,0 +1,63 @@
+import { render, screen, waitFor } from '@testing-library/react'
+import type { Mocked } from 'vitest'
+import { mockReset } from 'vitest-mock-extended'
+import type { UserRepository } from '../domain/user-repository'
+import { UserPage } from '../ui/user.page'
+import { UserLocator } from './di/user-locator'
+describe('User page', () => {
+  beforeEach(() => {
+    mockReset(UserLocator.testRepository)
+  })
+  it('should render properly', async () => {
+    const repoMock = UserLocator.testRepository as Mocked<UserRepository>
+
+    repoMock.getUser.mockResolvedValue({
+      name: 'Alberto',
+      id: 0,
+      username: '',
+      email: '',
+      address: {
+        street: '',
+        city: '',
+        zipcode: '',
+      },
+      phone: '',
+      company: {
+        name: '',
+        catchPhrase: '',
+      },
+    })
+
+    render(<UserPage />)
+    waitFor(async () => {
+      const title = await screen.findByText('User Information')
+      expect(title).toBeInTheDocument()
+    })
+  })
+  it('should render properly', async () => {
+    const repoMock = UserLocator.testRepository as Mocked<UserRepository>
+
+    repoMock.getUser.mockResolvedValue({
+      name: 'Alberto',
+      id: 0,
+      username: '',
+      email: '',
+      address: {
+        street: '',
+        city: '',
+        zipcode: '',
+      },
+      phone: '',
+      company: {
+        name: '',
+        catchPhrase: '',
+      },
+    })
+
+    render(<UserPage />)
+    waitFor(async () => {
+      const name = await screen.findByText('Name: Alberto')
+      expect(name).toBeInTheDocument()
+    })
+  })
+})
